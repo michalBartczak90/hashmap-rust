48fcd8bbd9bb56427ab0591cfb49c82e9cdae84a
Michał Bartczak <michal.bartczak.90@gmail.com>
2023-12-27 22:04:56 +0100

    Entry API

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,63 @@ use std::mem;
 
 const INITIAL_NBUCKETS: usize = 1;
 
+pub struct OccupiedEntry<'a, K: 'a, V: 'a> {
+    entry: &'a mut (K, V),
+}
+
+pub struct VacantEntry<'a, K: 'a, V: 'a> {
+    key: K,
+    map: &'a mut HashMap<K, V>,
+    bucket: usize,
+}
+
+impl<'a, K: 'a, V: 'a> VacantEntry<'a, K, V> {
+    pub fn insert(self, value: V) -> &'a mut V
+        where
+            K: Hash + Eq
+    {
+        self.map.buckets[self.bucket].push((self.key, value));
+        self.map.items += 1;
+        &mut self.map.buckets[self.bucket].last_mut().unwrap().1
+    }
+}
+
+pub enum Entry<'a, K: 'a, V: 'a>
+    where K: Hash + Eq
+{
+    Occupied(OccupiedEntry<'a, K, V>),
+    Vacant(VacantEntry<'a, K, V>),
+}
+
+impl<'a, K, V> Entry<'a, K, V>
+    where
+        K: Hash + Eq,
+{
+    pub fn or_insert(self, value: V) -> &'a mut V {
+        match self {
+            Entry::Occupied(e) => &mut e.entry.1,
+            Entry::Vacant(e) => e.insert(value),
+        }
+    }
+
+    pub fn or_insert_with<F>(self, maker: F) -> &'a mut V
+        where
+            F: FnOnce() -> V,
+    {
+        match self {
+            Entry::Occupied(e) => &mut e.entry.1,
+            Entry::Vacant(e) => e.insert(maker()),
+        }
+    }
+
+    pub fn or_default(self) -> &'a mut V
+        where
+            V: Default,
+    {
+        self.or_insert_with(Default::default)
+    }
+}
+
 pub struct HashMap<K, V> {
     buckets: Vec<Vec<(K, V)>>,
     items: usize,
@@ -22,6 +79,20 @@ impl<K, V> HashMap<K, V> {
 impl<K, V> HashMap<K, V>
     where K: Hash + Eq
 {
+    pub fn entry<'a>(&'a mut self, key: K) -> Entry<'a, K, V> {
+        if self.buckets.is_empty() || self.items > 3 * self.buckets.len() / 4 {
+            self.resize();
+        }
+        let bucket = self.bucket(&key);
+
+        for entry in &mut self.buckets[bucket] {
+            if entry.0 == key {
+                return Entry::Occupied(OccupiedEntry { entry: unsafe { &mut *(entry as *mut _) } });
+            }
+        }
+        Entry::Vacant(VacantEntry { key, map: self, bucket })
+    }
+
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
         if self.buckets.is_empty() || self.items > 3 * self.buckets.len() / 4 {
             self.resize();
